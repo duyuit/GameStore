a8bb789235218ea86f8fbfede163b105f94f2a2c
hoi <toilahoi007@gmail.com>
2018-09-23 19:42:23 +0700

    fix main program

---
+++ SteamDAL/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamDAL
+{
+    public static class ConsoleProgram
+    {
+        [STAThread]
+        static void Main()
+        {
+          
+        }
+    }
+}
